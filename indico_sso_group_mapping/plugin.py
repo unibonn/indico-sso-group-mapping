317526aa1df7cfaf387ca852c3b06dd8699e1286
Oliver Freyermuth <freyermuth@physik.uni-bonn.de>
2022-09-23 11:08:33 +0200

    plugin: Activate boolean cronjob config and logging statements.

--- indico_sso_group_mapping/plugin.py
+++ indico_sso_group_mapping/plugin.py
@@ -6,7 +6,7 @@
 
 #from indico.core.celery import celery
 
-#from from wtforms.fields import SelectField
+from from wtforms import BooleanField, SelectField
 from wtforms_sqlalchemy.fields import QuerySelectField
 
 from indico.core import signals
@@ -38,8 +38,8 @@ class SettingsForm(IndicoForm):
     sso_group = QuerySelectField('Local Users Group', allow_blank=True,
                                  query_factory=lambda: LocalGroup.query, get_label='name',
                                  description='The group to which anyone logging in with a matching SSO account is added.')
-    #enable_group_cleanup = BooleanField(_('Enable periodic Local Users Group cleanup'), widget=SwitchWidget(),
-    #                                  description=_('Enable periodic cleanup of Local Users Group for SSO accounts without login in the past year.'))
+    enable_group_cleanup = BooleanField(_('Enable periodic Local Users Group cleanup'), widget=SwitchWidget(),
+                                        description=_('Enable periodic cleanup of Local Users Group for SSO accounts without login in the past year.'))
 
 
 class SSOGroupMappingPlugin(IndicoPlugin):
@@ -52,7 +52,7 @@ class SSOGroupMappingPlugin(IndicoPlugin):
     settings_form = SettingsForm
     default_settings = {
         'sso_group': None,
-    #    'enable_group_cleanup': False,
+        'enable_group_cleanup': False,
     }
     settings_converters = {
         'sso_group': ModelConverter(LocalGroup),
@@ -74,8 +74,8 @@ class SSOGroupMappingPlugin(IndicoPlugin):
             return
         group = self.settings.get('sso_group')
         if not group:
-            #self.logger.warning('Local Users Group not set, plugin ineffective')
+            self.logger.warning('Local Users Group not set, plugin ineffective')
             return
         if identity.provider == 'uni-bonn-sso' and identity.identifier.endswith('@uni-bonn.de'):
-            #self.logger.info(f"Adding user with identity {identity.identifier} to local group {group}")
+            self.logger.info(f"Adding user with identity {identity.identifier} to local group {group}")
             group.members.add(user)
