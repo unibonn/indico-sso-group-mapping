e66c586e1737b714b1d03f9ef3a93232244fce70
Oliver Freyermuth <freyermuth@physik.uni-bonn.de>
2022-12-02 11:46:29 +0100

    plugin_test: Import plugin alone, not full plugin module.

--- tests/plugin_test.py
+++ tests/plugin_test.py
@@ -1,7 +1,7 @@
 import pytest
 
-from indico_sso_group_mapping import plugin
+from indico_sso_group_mapping.plugin import SSOGroupMappingPlugin
 
 def test_sso_group_mapping_plugin(app):
-    my_plugin = plugin.SSOGroupMappingPlugin(app)
+    my_plugin = SSOGroupMappingPlugin(app)
     assert my_plugin.configurable == True
