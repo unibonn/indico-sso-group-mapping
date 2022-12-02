650dff9613e20dbd92cfa07d2cbb0268cbf8472a
Oliver Freyermuth <freyermuth@physik.uni-bonn.de>
2022-12-02 13:01:45 +0100

    plugin_test: Import and pass in Indico plugin_engine.

--- tests/plugin_test.py
+++ tests/plugin_test.py
@@ -1,7 +1,9 @@
 import pytest
 
+from indico.core.plugins import plugin_engine
+
 from indico_sso_group_mapping.plugin import SSOGroupMappingPlugin
 
 def test_sso_group_mapping_plugin(app):
-    my_plugin = SSOGroupMappingPlugin(app)
+    my_plugin = SSOGroupMappingPlugin(plugin_engine, app)
     assert my_plugin.configurable == True
