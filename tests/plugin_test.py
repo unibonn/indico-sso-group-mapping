e4b97ab9a5a821c52d20192197e6a2f2e26fd425
Oliver Freyermuth <freyermuth@physik.uni-bonn.de>
2022-12-02 11:34:57 +0100

    plugin_test: Use "app" fixture.

--- tests/plugin_test.py
+++ tests/plugin_test.py
@@ -2,6 +2,6 @@ import pytest
 
 from indico_sso_group_mapping import plugin
 
-def test_sso_group_mapping_plugin():
-    my_plugin = plugin.SSOGroupMappingPlugin()
+def test_sso_group_mapping_plugin(app):
+    my_plugin = plugin.SSOGroupMappingPlugin(app)
     assert my_plugin.configurable == True
