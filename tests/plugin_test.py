b6392bccd53aebb6ef307114b3a63817cca23391
Oliver Freyermuth <freyermuth@physik.uni-bonn.de>
2022-12-23 11:45:55 +0100

    tests: Create a user with uni-bonn-sso identity.

--- tests/plugin_test.py
+++ tests/plugin_test.py
@@ -4,7 +4,16 @@ from indico.core.plugins import plugin_engine
 
 from indico_sso_group_mapping.plugin import SSOGroupMappingPlugin
 
+#from indico.modules.auth.models.identities import Identity
+from indico.modules.auth.util import save_identity_info
+
+from flask_multipass import IdentityInfo
 
 def test_sso_group_mapping_plugin(app):
     my_plugin = SSOGroupMappingPlugin(plugin_engine, app)
     assert my_plugin.configurable is True
+
+def test_create_sso_user(create_user):
+    user = create_user(1, email='foobar@uni-bonn.de')
+    identity_info = IdentityInfo('uni-bonn-sso', 'foobar@uni-bonn.de')
+    save_identity_info(identity_info, user)
