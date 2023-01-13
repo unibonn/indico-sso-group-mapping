9d336a82e4e39341658f1c38c3853d19d41017cf
Oliver Freyermuth <freyermuth@physik.uni-bonn.de>
2023-01-13 20:21:54 +0100

    ci: Create user group, configure it, and signal user login.

--- tests/plugin_test.py
+++ tests/plugin_test.py
@@ -1,8 +1,8 @@
 import os
 import pytest
 
+from indico.core import signals
 from indico.core.plugins import plugin_engine
-
 from indico_sso_group_mapping.plugin import SSOGroupMappingPlugin
 
 #from indico.modules.auth.models.identities import Identity
@@ -54,13 +54,24 @@ def test_sso_group_mapping_plugin(app):
     my_plugin = SSOGroupMappingPlugin(plugin_engine, app)
     assert my_plugin.configurable is True
 
-def test_create_sso_user(create_user):
+def test_create_sso_user(app, create_user, create_group):
     #FIXME: Need to import multipass from Indico!
     #identity_providers = multipass.identity_providers.values()
     # From this, get the active provider and pass it into IdentityInfo!
 
+    my_plugin = SSOGroupMappingPlugin(plugin_engine, app)
+    my_plugin.settings.set('sso_group', 'uni-bonn-users')
+
     user = create_user(1, email='foobar@uni-bonn.de')
     identity = Identity(user_id=1, provider='uni-bonn-sso', identifier='foobar@uni-bonn.de')
+    group = create_group(1)
+    group.name = 'uni-bonn-users'
+
+    assert('uni-bonn-users' not in user.local_groups)
+
+    signals.users.logged_in.send(user, identity=identity, admin_impersonation=False)
+
+    assert('uni-bonn-users' in user.local_groups)
 
     #identity_info = IdentityInfo('uni-bonn-sso', identifier='foobar@uni-bonn.de')
     #save_identity_info(identity_info, user)
