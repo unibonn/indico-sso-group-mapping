f54a77dbaba8e7ef1f4311dc0a7b4945ffb5ebfc
Oliver Freyermuth <freyermuth@physik.uni-bonn.de>
2023-01-14 20:42:28 +0100

    Add / fix headers in remaining files.

--- run-tests.sh
+++ run-tests.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-# This file is part of the Indico plugins.
-# Copyright (C) 2002 - 2022 CERN
+# This file is part of the Indico plugin indico-sso-group-mapping.
+# Copyright (C) 2022 - 2023 University of Bonn
 #
-# The Indico plugins are free software; you can redistribute
-# them and/or modify them under the terms of the MIT License;
+# The Indico plugin indico-sso-group-mapping is free software; you can
+# redistribute it and/or modify it under the terms of the MIT License;
 # see the LICENSE file for more details.
 
 for dir in $(find -name pytest.ini -exec dirname {} \;); do
--- setup.py
+++ setup.py
@@ -1,4 +1,9 @@
-# SPDX-License-Identifier: MIT
+# This file is part of the Indico plugin indico-sso-group-mapping.
+# Copyright (C) 2022 - 2023 University of Bonn
+#
+# The Indico plugin indico-sso-group-mapping is free software; you can
+# redistribute it and/or modify it under the terms of the MIT License;
+# see the LICENSE file for more details.
 
 from setuptools import setup
 
