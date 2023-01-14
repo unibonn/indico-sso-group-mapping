8fae44c646a6e4c2ac42151bacaa60aae7b473c5
Oliver Freyermuth <freyermuth@physik.uni-bonn.de>
2023-01-14 20:38:25 +0100

    Fix headers, 80 char width.

--- indico_sso_group_mapping/__init__.py
+++ indico_sso_group_mapping/__init__.py
@@ -1,9 +1,9 @@
 # This file is part of the Indico plugin indico-sso-group-mapping.
 # Copyright (C) 2022 - 2023 University of Bonn
 #
-# The Indico plugin indico-sso-group-mapping is free software; you can redistribute it and/or
-# modify it under the terms of the MIT License; see the
-# LICENSE file for more details.
+# The Indico plugin indico-sso-group-mapping is free software; you can
+# redistribute it and/or modify it under the terms of the MIT License;
+# see the LICENSE file for more details.
 
 from indico.core import signals
 from indico.util.i18n import make_bound_gettext
--- indico_sso_group_mapping/plugin.py
+++ indico_sso_group_mapping/plugin.py
@@ -1,9 +1,9 @@
 # This file is part of the Indico plugin indico-sso-group-mapping.
 # Copyright (C) 2022 - 2023 University of Bonn
 #
-# The Indico plugin indico-sso-group-mapping is free software; you can redistribute it and/or
-# modify it under the terms of the MIT License; see the
-# LICENSE file for more details.
+# The Indico plugin indico-sso-group-mapping is free software; you can
+# redistribute it and/or modify it under the terms of the MIT License;
+# see the LICENSE file for more details.
 
 from operator import attrgetter
 
