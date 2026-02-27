d26b713d85554cf3f42d3d186c4a82792930357d
Oliver Freyermuth <freyermuth@physik.uni-bonn.de>
2026-02-27 02:57:19 +0100

    bump copyright years

--- indico_sso_group_mapping/plugin.py
+++ indico_sso_group_mapping/plugin.py
@@ -1,5 +1,5 @@
 # This file is part of the Indico plugin indico-sso-group-mapping.
-# Copyright (C) 2022 - 2025 University of Bonn
+# Copyright (C) 2022 - 2026 University of Bonn
 #
 # The Indico plugin indico-sso-group-mapping is free software; you can
 # redistribute it and/or modify it under the terms of the MIT License;
